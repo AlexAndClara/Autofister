10cca927ac6df708c97232d2609b8a92584e402f
AlexAndClara <alex.shon.clara.columbina@gmail.com>
2026-06-21 17:49:08 +0000

    2000

--- streamlit_app.py
+++ streamlit_app.py
@@ -290,7 +290,7 @@ else:
     
     st.write(f"Du fik {score} ud af {total_questions} rigtige!")
     st.balloons()
-    additional_balloons = 200 - score  # More balloons for higher scores
+    additional_balloons = 2000 - score  # More balloons for higher scores
     for _ in range(additional_balloons):
         st.balloons()
     if score == total_questions:
